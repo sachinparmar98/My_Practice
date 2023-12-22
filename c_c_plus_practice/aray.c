b08a29997b7bd7a2a5dc8722bc6059834d0c0f96
sachinparmar98 <mr.sachinparmar98@gmail.com>
2023-12-22 22:32:01 +0530

    c question

---
+++ c_c_plus_practice/aray.c
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int arr[N];
+    for (int i = 0; i <= N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = N - 1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
