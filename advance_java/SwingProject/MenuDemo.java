f55bde9f9ec12cf7d56e1e90d4cd078666064964
sachinparmar98 <mr.sachinparmar98@gmail.com>
2024-02-02 16:26:02 +0530

    menuDemo modify

--- advance_java/SwingProject/MenuDemo.java
+++ advance_java/SwingProject/MenuDemo.java
@@ -98,36 +98,77 @@ class InsertDemo extends JPanel implements ActionListener {
     }
 }
 
-class SearchDemo extends JPanel {
-    JLabel u1, u2;
-    JTextField tx1, tx2;
+class SearchDemo extends JPanel implements ActionListener {
+    JLabel u1, u2, u3, u4, u5;
+    JTextField tx1, tx2, tx3, tx4, tx5;
     JButton b1;
-    JTextArea txa1;
 
     SearchDemo() {
         setLayout(null);
 
-        u1 = new JLabel("Enter Name");
-        u1.setBounds(50, 50, 120, 40);
+        u1 = new JLabel("Enter roll no.");
+        u1.setBounds(50, 50, 120, 30);
         add(u1);
         tx1 = new JTextField();
-        tx1.setBounds(200, 50, 180, 30);
+        tx1.setBounds(200, 50, 100, 30);
         add(tx1);
 
-        u2 = new JLabel("Enter RNo.");
-        u2.setBounds(50, 100, 120, 40);
+        b1 = new JButton("Search");
+        b1.setBounds(320, 50, 80, 30);
+        add(b1);
+        b1.addActionListener(this);
+
+        u2 = new JLabel("Student id");
+        u2.setBounds(50, 100, 120, 30);
         add(u2);
         tx2 = new JTextField();
-        tx2.setBounds(200, 100, 180, 30);
+        tx2.setBounds(200, 100, 100, 30);
         add(tx2);
 
-        b1 = new JButton("Search");
-        b1.setBounds(200, 180, 100, 30);
-        add(b1);
+        u3 = new JLabel("Physics marks");
+        u3.setBounds(50, 150, 120, 30);
+        add(u3);
+        tx3 = new JTextField();
+        tx3.setBounds(200, 150, 100, 30);
+        add(tx3);
 
-        txa1 = new JTextArea();
-        txa1.setBounds(80, 250, 300, 150);
-        add(txa1);
+        u4 = new JLabel("Chemistry marks");
+        u4.setBounds(50, 200, 120, 30);
+        add(u4);
+        tx4 = new JTextField();
+        tx4.setBounds(200, 200, 100, 30);
+        add(tx4);
+
+        u5 = new JLabel("Maths marks");
+        u5.setBounds(50, 250, 120, 30);
+        add(u5);
+        tx5 = new JTextField();
+        tx5.setBounds(200, 250, 100, 30);
+        add(tx5);
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String s1 = tx1.getText();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql:///schoolManagment", "root", "root");
+            Statement st = con.createStatement();
+            String q = "select * from insmark where UROLL='" + s1 + "'";
+            ResultSet rs = st.executeQuery(q);
+            if (rs.next()) {
+                tx2.setText(rs.getString(1));
+                tx3.setText(rs.getString(2));
+                tx4.setText(rs.getString(3));
+                tx5.setText(rs.getString(4));
+            } else {
+                JOptionPane.showMessageDialog(null, "invalid roll number");
+            }
+            con.close();
+
+        } catch (Exception e1) {
+            System.out.println(e1);
+        }
 
     }
 }
