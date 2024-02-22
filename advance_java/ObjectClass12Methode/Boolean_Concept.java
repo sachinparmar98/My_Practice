05bd334fcf4eaa206bb8d3c9d4e5d9674cb7d67e
sachinparmar98 <mr.sachinparmar98@gmail.com>
2024-02-22 23:07:55 +0530

    sachin rock

--- advance_java/ObjectClass12Methode/Boolean_Concept.java
+++ advance_java/ObjectClass12Methode/Boolean_Concept.java
@@ -1,23 +1,22 @@
 
-class Boolean_Concept
-{
-public static void main (String ar[])
-{
-Boolean a=new Boolean("ram"); 
-Boolean b=new Boolean("ram"); 
-Boolean c=new Boolean("sita"); 
+class Boolean_Concept {
+    public static void main(String ar[]) {
+        Boolean a = new Boolean("ram");
+        Boolean b = new Boolean("ram");
+        Boolean c = new Boolean("sita");
 
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
 
-System.out.println(a.equals(b));
-System.out.println(a.equals(c));
-
-}
+    }
 }
 
-/*out put
-true
-true
-
-
-Boolean ma true ka alawa kuch bhi likho too false hi manata ha means intrnaly voo ram sita ko false hi maan rha ha
-*/
+/*
+ * out put
+ * true
+ * true
+ * 
+ * sachin rock
+ * Boolean ma true ka alawa kuch bhi likho too false hi manata ha means intrnaly
+ * voo ram sita ko false hi maan rha ha
+ */
