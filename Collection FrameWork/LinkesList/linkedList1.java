647b5b9f42af944d0e638c740544939b042ef947
sachinparmar98 <mr.sachinparmar98@gmail.com>
2024-07-30 18:30:52 +0530

    add

---
+++ Collection FrameWork/LinkesList/linkedList1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class linkedList1 {
+    public static void main(String ar[]) {
+        LinkedList li = new LinkedList();
+        li.add(10);
+        li.add(20);
+        li.add(30);
+        li.add(40);
+        li.add(50);
+        System.out.println(li);
+    }
+}
