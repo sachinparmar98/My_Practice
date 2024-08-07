ef2ba4f337196a448378bab4f697a7d853fb7d43
sachinparmar98 <mr.sachinparmar98@gmail.com>
2024-08-07 19:49:40 +0530

    linked list file

--- Collection FrameWork/LinkesList/linkedList1.java
+++ Collection FrameWork/LinkesList/linkedList1.java
@@ -1,16 +1,20 @@
-import java.util.*;
+  //................ADD ELMENT INSIDE LINKEDLIST..........
+  
+ // import java.util.*;
 
-public class linkedList1 {
-    public static void main(String ar[]) {
-        LinkedList li = new LinkedList();
-        li.add(10);
-        li.add(20);
-        li.add(30);
-        li.add(40);
-        li.add(50);
-        System.out.println(li);
-    }
-}
+// public class linkedList1 {
+    // public static void main(String ar[]) {
+        // LinkedList li = new LinkedList();
+        // li.add(10);
+        // li.add(20);
+        // li.add(30);
+        // li.add(40);
+        // li.add(50);
+        // System.out.println(li);
+    // }
+// }
 
-output 
-[10, 20, 30, 40, 50]
+// output 
+// [10, 20, 30, 40, 50]
+
+//.........
---
+++ Collection FrameWork/Stack/MenuDriven.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class MenuDriven
+{
+public static void main(String ...a)
+{
+	LinkedList s=new LinkedList();
+	while(true)
+	{
+System.out.printf("............Menu.........\n 1 for InsertAtBegining\n 2 for InsertAtEnding \n 3 for Remove first\n 4 for Remove last \n 5 for getFrist \n 6 for getLast \n 7 for showAll \n 8 for exit\n");
+Scanner sc=new Scanner(System.in);
+byte b=sc.nextByte();
+int x=0;
+if(b==1 || b==2)
+{System.out.println("Enter data for insert\n");
+x=sc.nextInt();}
+switch(b)
+{
+	case 1 :s.addFirst(x);
+	break;
+	case 2 :s.addLast(x);
+	break;
+	case 3 :System.out.println(s.removeFirst());
+	break;
+	case 4: System.out.println(s.removeLast());
+    break;
+	case 5: System.out.println(s.getFirst());
+	break;
+	case 6: System.out.println(s.getLast());
+	break;
+	case 7: System.out.println(s);
+	break;
+	case 8: System.exit(0);
+	default:
+	System.out.println("invalid case label");
+}
+}
+}
+}
